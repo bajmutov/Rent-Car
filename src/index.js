2a51e2e37636f0f72c07546b457c52d589afb4b0
unknown <bajmutov@gmail.com>
2024-01-31 12:31:26 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { store } from 'redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+    <Provider store={null}>
+      <BrowserRouter basename="/Rent-Car">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
